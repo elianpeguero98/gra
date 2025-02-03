e1dae36dadd623b3515254c3d5b4092decc12cdd
elianpeguero98 <elianpeguero98@gmail.com>
2025-02-03 12:56:52 -0500

    Add files via upload

---
+++ gra2727p1EzequielPeguero2025/header.php
@@ -0,0 +1,3 @@
+ <header>
+    <h1><img src="Screenshot%202025-02-01%20083644.png" alt="" width="1209" height="871" class="logo"><img src="Screenshot%202025-02-02%20150921.png" alt="" width="200" height="66" class="media"></h1>
+  </header>
---
+++ gra2727p1EzequielPeguero2025/menu.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Watson Food Truck</title>
+<link href="style.css" rel="stylesheet" type="text/css">
+</head>
+
+<body>
+<wrapper>
+ <?php include"header.php"; ?>
+ <?php include"nav.php"; ?>
+  <main>
+  <h2 class="title">This Menu</h2>
+  <img src="menu.png" alt="" width="1501" height="845" class="menu"><br><img src="watson%20header.jpeg" alt="" width="2880" height="783" class="watson_header"></main>
+  <?php include"footer.php"; ?>
+</wrapper>
+</body>
+</html>
